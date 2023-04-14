aeca0f4974b50e61be6c0acab4d392e4b4c31c6d
RajPorus19 <raj.porus.hiruthayaraj@gmail.com>
2023-04-14 13:34:45 +0200

    jwt works, first implementation

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import jwt_utils
 
 app = Flask(__name__)
 
@@ -10,6 +11,12 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+@app.route('/login', methods=['POST'])
+def Login():
+    payload = request.get_json()
+    token = jwt_utils.build_token()
+    return {"token":token}, 200
 
 if __name__ == "__main__":
     app.run()
